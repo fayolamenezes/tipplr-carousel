f78f205c89b5595c5e26a1b51c09dbd9ccde1811
Fayola Menezes <fayolamenezes16@gmail.com>
2025-05-30 22:50:32 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const swiper = new Swiper(".mySwiper", {
   loop: true,
   autoHeight: true,
+  slidesPerView: 1,       // show 1 slide on mobile by default
+  spaceBetween: 20,       // space between slides
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -8,5 +10,11 @@ const swiper = new Swiper(".mySwiper", {
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,   // show 3 slides on desktop
+      spaceBetween: 30,
+    },
   },
 });
